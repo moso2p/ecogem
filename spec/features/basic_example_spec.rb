925ec86f89ce2e315cd871b9df7f1f48b6a69050
mosop <mosop@mosop.me>
2015-01-14 01:38:27 +0900

    refactoring

--- spec/features/basic_example_spec.rb
+++ spec/features/basic_example_spec.rb
@@ -1,10 +1,16 @@
+require 'fileutils'
+require 'tmpdir'
+
 RSpec.describe "basic example" do
   it do
-    Dir.chdir(File.expand_path('../basic_example', __FILE__)) do |dir|
-      Ecogem.new_workspace(nil) do |ws|
-        gemfile = "--gemfile=#{ws.gemfile.write.inspect}"
-        ws.gitsfile.save
-        expect(File.read('Gemfile').strip).to eq(File.read('expected').strip)
+    dir = File.expand_path('../basic_example', __FILE__)
+    Dir.mktmpdir(nil, dir) do |tmpdir|
+      FileUtils.cp "#{dir}/Ecogemfile", "#{tmpdir}/Ecogemfile"
+      Dir.chdir(tmpdir) do
+        Ecogem.new_workspace(nil) do |ws|
+          ws.gemfile.write
+          expect(File.read("#{tmpdir}/Gemfile").strip).to eq(File.read("#{dir}/expected").strip)
+        end
       end
     end
   end
