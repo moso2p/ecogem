f37cfe994c9b0cae607c7353b4c83d189f2d6cf9
mosop <mosop@mosop.me>
2015-01-14 01:22:25 +0900

    removed unnecessary code

--- tasks/test.rb
+++ tasks/test.rb
@@ -1,3 +1,2 @@
 require_relative 'test/all'
 require_relative 'test/install_bundlers'
-require_relative 'test/one'
--- tasks/test/one.rb
+++
@@ -1,8 +0,0 @@
-namespace :test do
-  desc "execute one of bundler versions"
-  task :one do
-    ver = ENV['ECOGEM_TEST_BUNDLER_VERSION']
-    ::Kernel.__send__ :gem, 'bundler', "= #{ver}"
-  end
-end
-
